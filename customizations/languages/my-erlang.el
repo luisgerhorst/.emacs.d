d6705191d158f072d75ffc64c4dfb310afbdc969
Luis Gerhorst <luis@luisgerhorst.de>
2015-02-18 18:14:37 +0100

    Cleaned up Erlang config Paredit for Erlang

--- customizations/languages/my-erlang.el
+++ customizations/languages/my-erlang.el
@@ -1,20 +1,15 @@
-;; Erlang with Homebrew paths.
-;; (setq erlang-root-dir "/usr/local/Cellar/erlang/17.3.4/lib/erlang")
-;; Redundant because erlang should already be in the exec-path from shell, but
-;; that's ok.
-;; (setq exec-path (cons "/usr/local/Cellar/erlang/17.3.4/lib/erlang/bin" exec-path))
-
-;; via MELPA
-;; (require 'erlang-start)
-
 ;; When erlang-mode:
 (add-hook 'erlang-mode-hook 'my/erlang-mode-hook)
 (defun my/erlang-mode-hook ()
+
   ;; Indent newlines. Using something like
   ;; (global-set-key (kbd "RET") 'newline-and-indent) 
   ;; doesn't work with erlang-mode.
   (local-set-key (kbd "RET") '(lambda ()(interactive)(newline)(indent-for-tab-command)))
-  (auto-highlight-symbol-mode t))
+
+  ;; No spaces when inserting parenthesis
+  (setq-local parens-require-space nil)
+  (setq-local parens-require-spaces nil))
 
 ;; When in erlang-shell-mode:
 (add-hook 'erlang-shell-mode-hook 'my/erlang-shell-mode-hook)
@@ -23,16 +18,12 @@
   (local-set-key (kbd "<up>") 'comint-previous-input)
   (local-set-key (kbd "<down>") 'comint-next-input))
 
-;; Minor modes for erlang-mode. May require edts to work nicely.
+;; Minor modes for erlang-mode.
 (add-hook 'erlang-mode-hook 'my/comment-auto-fill)
+(add-hook 'erlang-mode-hook #'enable-paredit-mode)
 
 ;; Reindent yanked lines
 (add-to-list 'yank-indent-modes 'erlang-mode)
 
-;; Not enabled because of flackering cursor when frame is split.
-;; (add-hook 'erlang-mode-hook #'auto-complete-mode)
-
 ;; EDTS
 (require 'edts-start)
-;; (add-hook 'edts-mode-hook 'my/edts-mode-hook)
-;; (defun my/edts-mode-hook ())
