ed7e894e443f7d84139be3944c045e29dda6f69d
Luis Gerhorst <luis@luisgerhorst.de>
2015-02-19 18:45:55 +0100

    ParEdit with Erlang: No Lisp specific comment-dwim

--- customizations/languages/my-erlang.el
+++ customizations/languages/my-erlang.el
@@ -1,20 +1,3 @@
-;; When erlang-mode:
-(add-hook 'erlang-mode-hook 'my/erlang-mode-hook)
-(defun my/erlang-mode-hook ()
-
-  ;; Indent newlines. Using something like
-  ;; (global-set-key (kbd "RET") 'newline-and-indent) 
-  ;; doesn't work with erlang-mode.
-  (local-set-key (kbd "RET") '(lambda ()(interactive)(newline)(indent-for-tab-command)))
-
-  ;; No spaces when inserting parenthesis
-  (setq-local parens-require-space nil)
-  (setq-local parens-require-spaces nil)
-
-  (defun my/erlang-paredit-space-for-delimiter-p (endp delimiter)
-    nil)
-  (setq-local paredit-space-for-delimiter-predicates '(my/erlang-paredit-space-for-delimiter-p)))
-
 ;; When in erlang-shell-mode:
 (add-hook 'erlang-shell-mode-hook 'my/erlang-shell-mode-hook)
 (defun my/erlang-shell-mode-hook ()
@@ -24,11 +7,18 @@
 
 ;; Minor modes for erlang-mode.
 (add-hook 'erlang-mode-hook 'my/comment-auto-fill)
-(add-hook 'erlang-mode-hook #'enable-paredit-mode)
 (add-hook 'erlang-mode-hook #'aggressive-indent-mode)
 
-;; Reindent yanked lines
-(add-to-list 'yank-indent-modes 'erlang-mode)
+;; ParEdit
+(add-hook 'erlang-mode-hook #'enable-paredit-mode)
+(add-hook 'erlang-mode-hook 'my/erlang-paredit-mode-hook)
+(defun my/erlang-paredit-mode-hook ()
+  ;; No space when inserting parentheses.
+  (defun my/erlang-paredit-space-for-delimiter-p (endp delimiter) nil)
+  (setq-local paredit-space-for-delimiter-predicates '(my/erlang-paredit-space-for-delimiter-p))
+  
+  ;; No paredit-comment-dwim (inserts lisp comments).
+  (local-set-key [remap paredit-comment-dwim] 'comment-dwim))
 
 ;; EDTS
 (require 'edts-start)
