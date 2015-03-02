7039e6ed17af36f4cdffaa8bf5a1893b23fe3fcc
Luis Gerhorst <luis@luisgerhorst.de>
2015-03-02 16:54:29 +0100

    Evil leader and principles for using Evil commands

--- customizations/my-vim.el
+++ customizations/my-vim.el
@@ -1,3 +1,11 @@
+;; Only use Evil for text editing. Not for stuff like navigating to a file,
+;; switching buffers, etc. This way Emacs stays useable for you if you're in a
+;; buffer that is not used for text editing (e.g. magit, paradox or some shell).
+
+;; Reminder: Only editing commands.
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
 (require 'evil)
 (evil-mode 1)
 
