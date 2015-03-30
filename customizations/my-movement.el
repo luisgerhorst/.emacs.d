71fea83a7587e2730a44e215c9573e6d4bb94cf3
Luis Gerhorst <luis@luisgerhorst.de>
2015-03-30 14:50:51 +0200

    Automatically wrap isearch

--- customizations/my-movement.el
+++ customizations/my-movement.el
@@ -39,3 +39,12 @@
 
 ;; Jump directly to a word beginning with a given char.
 (global-set-key (kbd "C-ö") 'ace-jump-mode)
+
+;; Automatically wrap isearch.
+(defadvice isearch-search (after isearch-no-fail activate)
+  (unless isearch-success
+    (ad-disable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)
+    (isearch-repeat (if isearch-forward 'forward))
+    (ad-enable-advice 'isearch-search 'after 'isearch-no-fail)
+    (ad-activate 'isearch-search)))
