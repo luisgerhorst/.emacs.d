fcf1d8eab17bf68a133749263149622eb340be36
Luis Gerhorst <luis@luisgerhorst.de>
2015-06-07 20:05:18 +0200

    Auto-start in fullscreen; Enable mouse

--- customizations/my-ui.el
+++ customizations/my-ui.el
@@ -8,6 +8,7 @@
 ;; values in order to set the width (in characters wide) and height
 ;; (in lines high) Emacs will have whenever you start it
 (setq initial-frame-alist '((top . 0) (left . 0) (width . 317) (height . 81)))
+(set-frame-parameter nil 'fullscreen 'fullboth)
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
@@ -45,12 +46,12 @@
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 1)))
 
 ;; Disable Mouse
-(dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
-             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
-             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
-             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
-             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
-  (global-unset-key k))
+;; (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+;;              [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+;;              [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+;;              [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+;;              [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+;;   (global-unset-key k))
 
 ;; Highlights matching parenthesis
 (show-paren-mode t)
