8d27cd55647a166a1c81f10f04c1043a2a0b331d
Luis Gerhorst <luis@luisgerhorst.de>
2015-10-25 14:21:59 +0100

    Filling for markdown files

--- customizations/languages/my-markdown.el
+++ customizations/languages/my-markdown.el
@@ -2,13 +2,19 @@
 
 (require-package 'markdown-mode)
 
+;; Can be enabled in files with unfilled lines.
+(define-key markdown-mode-map (kbd "C-c f") 'buffer-face-mode)
+
 (defun my/markdown-mode-hook ()
-  (setq buffer-face-mode-face '(:family "Input Serif"))
-  (buffer-face-mode 1)
+  (setq-local buffer-face-mode-face '(:family "Input Serif"))
+  (buffer-face-mode -1)
+
   (show-paren-mode -1)
   (visual-line-mode 1)
   (auto-fill-mode 1))
+
 (add-hook 'markdown-mode-hook 'my/markdown-mode-hook)
+
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
