f72bb73f5a13d7fc13bf7a55f35b7a1bc1a071a2
Luis Gerhorst <privat@luisgerhorst.de>
2015-11-09 10:13:47 +0100

    Disable subword-mode for reStructuredText files

--- customizations/languages/my-rst.el
+++ customizations/languages/my-rst.el
@@ -5,5 +5,6 @@
 (defun my/rst-mode-hook ()
   (show-paren-mode -1)
   (visual-line-mode 1)
-  (auto-fill-mode 1))
+  (auto-fill-mode 1)
+  (subword-mode -1))
 (add-hook 'rst-mode-hook 'my/rst-mode-hook)
