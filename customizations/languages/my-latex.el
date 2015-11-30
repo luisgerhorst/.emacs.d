58747be253c1fbe45551fe67fc23f0f8644812dc
Luis Gerhorst <privat@luisgerhorst.de>
2015-11-30 20:59:36 +0100

    Proper LaTeX autocompletion
    Only complete LaTeX expressions and make suggestions immediatly.

--- customizations/languages/my-latex.el
+++ customizations/languages/my-latex.el
@@ -7,8 +7,19 @@
 (setq TeX-parse-self t)
 (setq TeX-newline-function 'newline-and-indent)
 
+
 (defun my/latex-mode-hook ()
   ;; Proper line wrapping for text.
   (visual-line-mode 1)
-  (auto-fill-mode 1))
+  (auto-fill-mode 1)
+  
+  ;; Only auto-complete using auctex backends (not regular words in
+  ;; text).
+  (setq company-backends '())
+  (company-auctex-init)
+  ;; Make suggestions immediately.
+  (setq company-dabbrev-time-limit 0.0)
+  (setq company-dabbrev-minimum-length 2)
+  (company-mode 1))
+
 (add-hook 'LaTeX-mode-hook 'my/latex-mode-hook)
