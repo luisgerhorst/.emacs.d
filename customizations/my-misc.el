7e2c0919e915c7882b027053ba30237e5427973e
Luis Gerhorst <privat@luisgerhorst.de>
2016-01-17 18:55:54 +0100

    Restore default behaviour on C-x C-c

--- customizations/my-misc.el
+++ customizations/my-misc.el
@@ -1,13 +1,1 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
-
-;;; Call when you are done with a task. Better than quitting Emacs.
-
-(defun end-emacs-session ()
-  (interactive)
-  (save-some-buffers)
-  (delete-other-windows)
-  (command-execute 'bookmark-bmenu-list)
-  (ns-do-hide-emacs))
-
-(global-set-key [remap save-buffers-kill-terminal] 'end-emacs-session)
-(global-set-key (kbd "C-c c") 'save-buffers-kill-emacs)
