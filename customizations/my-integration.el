5c1cf85d216bc11c425ae1aeb2d4dd5ee705ab71
Luis Gerhorst <privat@luisgerhorst.de>
2016-01-19 22:50:15 +0100

    Do not quit Emacs when last OS X window is closed

--- customizations/my-integration.el
+++ customizations/my-integration.el
@@ -77,3 +77,29 @@ end tell
 
 (when (eq system-type 'darwin)
   (global-set-key [remap suspend-frame] 'ns-do-hide-emacs))
+
+;; Directly copied from frame.el but now hide Emacs instead of killing
+;; it when last frame will be closed.
+;; TODO: Replace ns-do-hide-emacs with a function that does everything
+;; that would also happen if Emacs is closed and reopened, aka
+;; save-some-buffers, revert all unsaved buffers, reset the frame to the
+;; default configuration and finally ns-do-hide-emacs.
+(defun handle-delete-frame-without-kill-emacs (event)
+  "Handle delete-frame events from the X server."
+  (interactive "e")
+  (let ((frame (posn-window (event-start event)))
+        (i 0)
+        (tail (frame-list)))
+    (while tail
+      (and (frame-visible-p (car tail))
+           (not (eq (car tail) frame))
+           (setq i (1+ i)))
+      (setq tail (cdr tail)))
+    (if (> i 0)
+        (delete-frame frame t)
+      ;; Not (save-buffers-kill-emacs) but instead:
+      (ns-do-hide-emacs))))
+
+(when (eq system-type 'darwin)
+  (advice-add 'handle-delete-frame :override
+              #'handle-delete-frame-without-kill-emacs))
