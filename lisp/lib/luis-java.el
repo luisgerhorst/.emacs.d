92d5d35080e1b550a0cf2fc1ffcdd7507cd47432
Luis Gerhorst <privat@luisgerhorst.de>
2016-05-24 15:15:21 +0200

    Ergonomic keybinding for dabbrev-expand in JDEE

--- lisp/lib/luis-java.el
+++ lisp/lib/luis-java.el
@@ -3,6 +3,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.java\\'" . jdee-mode))
 
-
+(add-hook 'jdee-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-i") 'dabbrev-expand)))
 
 (provide 'luis-java)
