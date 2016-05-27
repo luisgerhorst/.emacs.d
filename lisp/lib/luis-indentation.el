ef0180ff2afc628cbbb33d811370a323b3ad67dc
Luis Gerhorst <privat@luisgerhorst.de>
2016-05-27 19:57:30 +0200

    Refine Java and C automatic indentation

--- lisp/lib/luis-indentation.el
+++ lisp/lib/luis-indentation.el
@@ -4,5 +4,14 @@
 (require 'aggressive-indent)
 (global-aggressive-indent-mode 1)
 
+(add-to-list
+ 'aggressive-indent-dont-indent-if
+ ;; Don't indent in Java and C files when line does not contain a
+ ;; semicolon or code block bracket.
+ '(and (or (derived-mode-p 'jdee-mode)
+           (derived-mode-p 'c-mode))
+       (null (string-match "[;{}]"
+                           (thing-at-point 'line)))))
+
 
 (provide 'luis-indentation)
