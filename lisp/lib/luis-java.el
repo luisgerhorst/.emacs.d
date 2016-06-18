02a0fb099c78429bb69f49697e0ae5c3e876125b
Luis Gerhorst <privat@luisgerhorst.de>
2016-06-19 00:05:14 +0200

    Fix jdee mode

--- lisp/lib/luis-java.el
+++ lisp/lib/luis-java.el
@@ -1,9 +1,9 @@
 (use-package jdee
   :ensure t
-  :mode "\\.java\\'"
-  :commands (jdee-mode))
-
-(add-hook 'jdee-mode-hook #'company-mode)
+  :mode ("\\.java\\'" . jdee-mode)
+  :commands (jdee-mode)
+  :config
+  (add-hook 'jdee-mode-hook #'company-mode))
 
 
 (provide 'luis-java)
