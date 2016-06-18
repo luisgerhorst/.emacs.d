44a4b7bb9ad4dadc6596f3d6c61419ac18a69383
Luis Gerhorst <privat@luisgerhorst.de>
2016-06-18 20:43:20 +0200

    add-hook calls outside use-package :init

--- lisp/lib/luis-elisp.el
+++ lisp/lib/luis-elisp.el
@@ -1,22 +1,21 @@
 (use-package paredit
-  :commands (enable-paredit-mode)
-  :init
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
+  :ensure t
+  :commands (enable-paredit-mode))
 
-(progn
-  ;; eldoc-mode shows documentation in the minibuffer when writing code
-  ;; http://www.emacswiki.org/emacs/ElDoc
-  (add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
-  (add-hook 'lisp-interaction-mode-hook #'eldoc-mode)
-  (add-hook 'ielm-mode-hook #'eldoc-mode))
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook #'enable-paredit-mode)
+
+;; eldoc-mode shows documentation in the minibuffer when writing code
+;; http://www.emacswiki.org/emacs/ElDoc
+(add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook #'eldoc-mode)
+(add-hook 'ielm-mode-hook #'eldoc-mode)
 
 (add-hook 'emacs-lisp-mode-hook #'company-mode-on)
-
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c e") 'eval-buffer)))
