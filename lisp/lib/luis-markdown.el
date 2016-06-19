e1b4b67ed80fbfce9c13b993cbb01006441bdb85
Luis Gerhorst <privat@luisgerhorst.de>
2016-06-19 11:55:15 +0200

    Use use-package :init

--- lisp/lib/luis-markdown.el
+++ lisp/lib/luis-markdown.el
@@ -1,23 +1,21 @@
 (use-package markdown-mode
   :ensure t
+  :init
+  (defun luis-markdown-mode-hook ()
+    ;; Can be enabled in files with unfilled lines.
+    (setq-local buffer-face-mode-face '(:family "Input Serif"))
+    (buffer-face-mode -1)
+    (local-set-key (kbd "C-c f") 'buffer-face-mode)
+
+    ;; Disable modes usefull for programming.
+    (show-paren-mode -1)
+    (subword-mode -1))
+
+  (add-hook 'markdown-mode-hook #'luis-markdown-mode-hook)
+  ;; Proper line wrapping for text.
+  (add-hook 'markdown-mode-hook #'visual-fill-column-mode)
   :mode ("\\.md\\'"
          "\\.markdown\\'"))
-
-(defun luis-markdown-mode-hook ()
-  ;; Can be enabled in files with unfilled lines.
-  (setq-local buffer-face-mode-face '(:family "Input Serif"))
-  (buffer-face-mode -1)
-  (local-set-key (kbd "C-c f") 'buffer-face-mode)
-
-  ;; Disable modes usefull for programming.
-  (show-paren-mode -1)
-  (subword-mode -1)
-
-  ;; Proper line wrapping for text.
-  (visual-line-mode 1)
-  (visual-fill-column-mode 1))
-
-(add-hook 'markdown-mode-hook #'luis-markdown-mode-hook)
 
 
 (provide 'luis-markdown)
