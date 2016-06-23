d87bf0c6152789ffcc4e56dffccc686bdd2edc30
Luis Gerhorst <privat@luisgerhorst.de>
2016-06-23 22:38:33 +0200

    Easily toggle (nice) line wrapping with C-c w

--- lisp/luis-look.el
+++ lisp/luis-look.el
@@ -10,11 +10,21 @@
   :ensure t
   :commands (adaptive-wrap-prefix-mode)
   :config
-  (setq-default adaptive-wrap-extra-indent 2)
-  (add-hook 'adaptive-wrap-prefix-mode-hook #'visual-line-mode))
+  (setq-default adaptive-wrap-extra-indent 2))
+
+(defun luis-toggle-wrapping ()
+  (interactive)
+  (if truncate-lines
+      (progn
+        (adaptive-wrap-prefix-mode 1)
+        ;; This also disables truncate-lines:
+        (visual-line-mode 1))
+    (adaptive-wrap-prefix-mode -1)
+    (visual-line-mode -1)
+    (kill-local-variable 'truncate-lines)))
 
 ;; Enable in buffer with unfilled lines (that you can't edit).
-(global-set-key (kbd "C-c w") #'adaptive-wrap-prefix-mode)
+(global-set-key (kbd "C-c w") #'luis-toggle-wrapping)
 
 ;;; Theme
 
