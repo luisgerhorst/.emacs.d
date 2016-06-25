bb0a0d7901abbcc88a15232244a362dc0191751b
Luis Gerhorst <privat@luisgerhorst.de>
2016-06-25 16:21:17 +0200

    Auto compile elisp on save

--- lisp/lib/luis-elisp.el
+++ lisp/lib/luis-elisp.el
@@ -26,5 +26,26 @@
 
 (add-to-list 'auto-mode-alist '("\\.el.template\\'" . emacs-lisp-mode))
 
+(defun test-emacs-config ()
+  "Start shell Emacs in background to test config."
+  (interactive)
+  (require 'async)
+  (async-start
+   (lambda () (shell-command-to-string
+               "emacs --batch --eval \"(condition-case e (progn (load \\\"~/.emacs.d/init.el\\\") (message \\\"-OK-\\\")) (error (message \\\"ERROR!\\\") (signal (car e) (cdr e))))\""))
+   `(lambda (output)
+      (if (string-match "-OK-" output)
+          (when ,(called-interactively-p 'any)
+            (message "Emacs config ok"))
+        (switch-to-buffer-other-window "*startup error*")
+        (delete-region (point-min) (point-max))
+        (insert output)
+        (search-backward "ERROR!")))))
+
+(use-package auto-compile
+  :ensure t
+  :config
+  (auto-compile-on-save-mode))
+
 
 (provide 'luis-elisp)
--- lisp/luis-misc.el
+++ lisp/luis-misc.el
@@ -1,19 +1,3 @@
-(defun test-emacs-config ()
-  "Start shell Emacs in background to test config."
-  (interactive)
-  (require 'async)
-  (async-start
-   (lambda () (shell-command-to-string
-               "emacs --batch --eval \"(condition-case e (progn (load \\\"~/.emacs.d/init.el\\\") (message \\\"-OK-\\\")) (error (message \\\"ERROR!\\\") (signal (car e) (cdr e))))\""))
-   `(lambda (output)
-      (if (string-match "-OK-" output)
-          (when ,(called-interactively-p 'any)
-            (message "Emacs config ok"))
-        (switch-to-buffer-other-window "*startup error*")
-        (delete-region (point-min) (point-max))
-        (insert output)
-        (search-backward "ERROR!")))))
-
 ;; Bookmark on startup.
 (setq initial-buffer-choice
 	  (lambda ()
