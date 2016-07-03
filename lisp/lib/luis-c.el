13eca09e652333f778d26da3209e9918d90e5f3e
Luis Gerhorst <privat@luisgerhorst.de>
2016-07-03 13:48:19 +0200

    C Headers autocompletion

--- lisp/lib/luis-c.el
+++ lisp/lib/luis-c.el
@@ -2,5 +2,12 @@
 (add-hook 'c-mode-hook
           #'luis-company-configure-automatic-completion)
 
+(use-package company-c-headers
+  :ensure t
+  :after (company)
+  :commands (company-c-headers)
+  :init
+  (add-to-list 'company-backends #'company-c-headers))
+
 
 (provide 'luis-c)
