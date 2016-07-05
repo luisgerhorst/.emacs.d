5aa947068ce10af8b1de44448549466280525096
Luis Gerhorst <privat@luisgerhorst.de>
2016-07-05 20:05:28 +0200

    Set C indentation style to K&R

--- lisp/lib/luis-c.el
+++ lisp/lib/luis-c.el
@@ -1,6 +1,12 @@
 (setq c-basic-offset tab-width)
-(add-hook 'c-mode-hook
-          #'luis-company-configure-automatic-completion)
+
+;; Set default indentation style to K&R instead of weird GNU style. See
+;; https://www.emacswiki.org/emacs/IndentingC#toc2
+(unless (listp c-default-style)
+  (setq c-default-style nil))
+(add-to-list 'c-default-style '(other . "k&r"))
+
+(add-hook 'c-mode-hook #'luis-company-configure-automatic-completion)
 
 (use-package company-c-headers
   :ensure t
