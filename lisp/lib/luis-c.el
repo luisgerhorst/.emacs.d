01c8cc2348fbdd130397541244e9b2339b8ef2ad
Luis Gerhorst <privat@luisgerhorst.de>
2016-07-06 11:35:36 +0200

    use-package cc-mode

--- lisp/lib/luis-c.el
+++ lisp/lib/luis-c.el
@@ -1,12 +1,13 @@
-(setq c-basic-offset tab-width)
+(use-package cc-mode
+  :config
+  (setq c-basic-offset tab-width)
+  (add-hook 'c-mode-hook #'luis-company-configure-automatic-completion)
 
-;; Set default indentation style to K&R instead of weird GNU style. See
-;; https://www.emacswiki.org/emacs/IndentingC#toc2
-(unless (listp c-default-style)
-  (setq c-default-style nil))
-(add-to-list 'c-default-style '(other . "k&r"))
-
-(add-hook 'c-mode-hook #'luis-company-configure-automatic-completion)
+  ;; Set default indentation style to K&R instead of weird GNU style. See
+  ;; https://www.emacswiki.org/emacs/IndentingC#toc2
+  (unless (listp c-default-style)
+    (setq c-default-style nil))
+  (add-to-list 'c-default-style '(other . "k&r")))
 
 (use-package company-c-headers
   :ensure t
