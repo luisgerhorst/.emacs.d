3b786fb5212f59ec56075edb582224d1a20fb598
Luis Gerhorst <privat@luisgerhorst.de>
2016-07-06 12:35:06 +0200

    Add melpa-stable

--- lisp/luis-packages.el
+++ lisp/luis-packages.el
@@ -5,9 +5,11 @@
 (add-to-list 'package-archives
              '("elpa" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
