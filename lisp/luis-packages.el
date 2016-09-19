16f1c44b24679fe95a3e98b27854fd7005e56a92
Luis Gerhorst <privat@luisgerhorst.de>
2016-09-19 15:40:20 +0200

    Remove useless code, order archives by priority

--- lisp/luis-packages.el
+++ lisp/luis-packages.el
@@ -3,13 +3,17 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("elpa" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+
+(setq package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("melpa" . 5)
+        ("gnu" . 0)
+        ("marmalade" . -5)))
 
 (package-initialize)
 
