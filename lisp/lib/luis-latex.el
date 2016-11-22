047ad4634ce93ec75eaf3bd222b948067ba74efe
Luis Gerhorst <privat@luisgerhorst.de>
2016-11-22 13:33:22 +0100

    use-package for AUCTeX

--- lisp/lib/luis-latex.el
+++ lisp/lib/luis-latex.el
@@ -1,20 +1,20 @@
-(require-package 'auctex)
+(use-package tex-site ;; AUCTeX overrides LaTeX
+  :ensure auctex
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq TeX-newline-function #'newline-and-indent)
+
+  (add-hook 'LaTeX-mode-hook #'visual-line-mode)
+  (add-hook 'LaTeX-mode-hook #'auto-fill-mode)
+  (add-hook 'LaTeX-mode-hook #'luis-company-configure-automatic-completion)
+  (add-hook 'LaTeX-mode-hook #'flycheck-mode))
 
 (use-package company-auctex
   :ensure t
   :config
   (company-auctex-init))
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq TeX-newline-function #'newline-and-indent)
-
-(add-hook 'LaTeX-mode-hook #'luis-latex-mode-hook)
-(add-hook 'LaTeX-mode-hook #'luis-company-configure-automatic-completion)
-
-(defun luis-latex-mode-hook ()
-  ;; Proper line wrapping for text.
-  (visual-line-mode 1)
-  (auto-fill-mode 1))
 
 (provide 'luis-latex)
