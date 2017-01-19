a66f1ac658a14bfade09f52acdc0e677d73b20d7
Luis Gerhorst <privat@luisgerhorst.de>
2017-01-19 21:50:15 +0100

    man replaces current buffer

--- lisp/luis-custom.el
+++ lisp/luis-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote pushy))
  '(ahs-default-range (quote ahs-range-whole-buffer))
  '(auto-coding-alist
    (quote
