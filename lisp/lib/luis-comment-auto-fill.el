8fb8f1c70d08e9f8f4a1bd19d852af5436d777a4
Luis Gerhorst <privat@luisgerhorst.de>
2017-05-16 21:56:09 +0200

    Fix Comment-Auto-Fill-Mode
    Disable/Enable code was switched

--- lisp/lib/luis-comment-auto-fill.el
+++ lisp/lib/luis-comment-auto-fill.el
@@ -1,16 +1,16 @@
 (require 'diminish)
 
 (define-minor-mode luis-comment-auto-fill-mode
-  "Nice line wrapping for text files."
+  "Auto fill only comments."
   :lighter " CommentFill"
   (if luis-comment-auto-fill-mode
       (progn
-        (kill-local-variable 'comment-auto-fill-only-comments)
-        (diminish-undo 'auto-fill-function)
-        (auto-fill-mode -1))
-    (setq-local comment-auto-fill-only-comments t)
-    (diminish 'auto-fill-function)
-    (auto-fill-mode 1)))
+        (setq-local comment-auto-fill-only-comments t)
+        (diminish 'auto-fill-function)
+        (auto-fill-mode 1))
+    (kill-local-variable 'comment-auto-fill-only-comments)
+    (diminish-undo 'auto-fill-function)
+    (auto-fill-mode -1)))
 
 
 (provide 'luis-comment-auto-fill)
