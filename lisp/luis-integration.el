a9e00fd4cbf53ffcfd9da0b278d2568adf911010
Luis Gerhorst <privat@luisgerhorst.de>
2017-05-21 22:20:39 +0200

    Remove code to hide emacs

--- lisp/luis-integration.el
+++ lisp/luis-integration.el
@@ -29,8 +29,8 @@
   (let ((finder-dir (do-applescript "tell application \"Finder\"\nreturn POSIX path of (target of window 1 as alias)\nend tell")))
     (ido-find-file-in-dir finder-dir)))
 
-(when (eq system-type 'darwin)
-  (global-set-key [remap suspend-frame] 'ns-do-hide-emacs))
+;; (when (eq system-type 'darwin)
+;;   (global-set-key [remap suspend-frame] 'ns-do-hide-emacs))
 
 ;; Directly copied from frame.el but now hide Emacs instead of killing
 ;; it when last frame will be closed.
@@ -38,25 +38,25 @@
 ;; that would also happen if Emacs is closed and reopened, aka
 ;; save-some-buffers, revert all unsaved buffers, reset the frame to the
 ;; default configuration and finally ns-do-hide-emacs.
-(defun handle-delete-frame-without-kill-emacs (event)
-  "Handle delete-frame events from the X server."
-  (interactive "e")
-  (let ((frame (posn-window (event-start event)))
-        (i 0)
-        (tail (frame-list)))
-    (while tail
-      (and (frame-visible-p (car tail))
-           (not (eq (car tail) frame))
-           (setq i (1+ i)))
-      (setq tail (cdr tail)))
-    (if (> i 0)
-        (delete-frame frame t)
-      ;; Not (save-buffers-kill-emacs) but instead:
-      (ns-do-hide-emacs))))
+;; (defun handle-delete-frame-without-kill-emacs (event)
+;;   "Handle delete-frame events from the X server."
+;;   (interactive "e")
+;;   (let ((frame (posn-window (event-start event)))
+;;         (i 0)
+;;         (tail (frame-list)))
+;;     (while tail
+;;       (and (frame-visible-p (car tail))
+;;            (not (eq (car tail) frame))
+;;            (setq i (1+ i)))
+;;       (setq tail (cdr tail)))
+;;     (if (> i 0)
+;;         (delete-frame frame t)
+;;       ;; Not (save-buffers-kill-emacs) but instead:
+;;       (ns-do-hide-emacs))))
 
-(when (eq system-type 'darwin)
-  (advice-add 'handle-delete-frame :override
-              #'handle-delete-frame-without-kill-emacs))
+;; (when (eq system-type 'darwin)
+;;   (advice-add 'handle-delete-frame :override
+;;               #'handle-delete-frame-without-kill-emacs))
 
 
 (provide 'luis-integration)
