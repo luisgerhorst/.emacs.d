8c3dbd2e438d0cc85e5a0db8dde167db3c9c5d23
Luis Gerhorst <privat@luisgerhorst.de>
2017-05-22 18:13:17 +0200

    Enable desktop-save-mode for frame/window configuration

--- lisp/luis-misc.el
+++ lisp/luis-misc.el
@@ -4,5 +4,10 @@
 		(bookmark-bmenu-list)
         (get-buffer "*Bookmark List*")))
 
+(setq desktop-buffers-not-to-save ".*"
+      desktop-files-not-to-save ".*"
+      desktop-globals-to-save nil
+      desktop-load-locked-desktop t)
+(desktop-save-mode 1)
 
 (provide 'luis-misc)
