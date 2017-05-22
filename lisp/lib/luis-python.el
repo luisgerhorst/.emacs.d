2382f5a568c1ab7cb884151ff724cf65a980ba7c
Luis Gerhorst <privat@luisgerhorst.de>
2017-05-22 22:58:59 +0200

    Use anaconda mode for Python jump to definition

--- lisp/lib/luis-python.el
+++ lisp/lib/luis-python.el
@@ -8,11 +8,27 @@
   (with-eval-after-load 'company
     (add-to-list 'company-backends #'company-jedi)))
 
+(use-package anaconda-mode
+  :defer t
+  :bind (:map
+         anaconda-mode-map
+         ;; Use company completion powered by jedi since its better.
+         ("C-M-i" . nil)
+         ("M-," . anaconda-mode-go-back)
+         ;; Equals M-Option-. on Mac
+         ("M-…" . anaconda-mode-find-assignments))
+  :init
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  ;; The eldoc function by anaconda mode almost never works, thus we leave it
+  ;; disabled.
+  )
+
 (defun luis-python-mode-hook ()
   (electric-indent-just-newline nil))
 
 (add-hook 'python-mode-hook #'flycheck-mode)
 (add-hook 'python-mode-hook #'luis-python-mode-hook)
 (add-hook 'python-mode-hook #'luis-company-configure-automatic-completion)
+
 
 (provide 'luis-python)
