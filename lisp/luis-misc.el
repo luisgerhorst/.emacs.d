52b03d07fd280d58e5ac67e5f1a48e7aee20a546
Luis Gerhorst <privat@luisgerhorst.de>
2017-05-22 19:08:00 +0200

    Always load desktop but don't autosave it when idle

--- lisp/luis-misc.el
+++ lisp/luis-misc.el
@@ -7,7 +7,9 @@
 (setq desktop-buffers-not-to-save ".*"
       desktop-files-not-to-save ".*"
       desktop-globals-to-save nil
-      desktop-load-locked-desktop t)
+      desktop-load-locked-desktop t
+      desktop-save t
+      desktop-auto-save-timeout nil)
 (desktop-save-mode 1)
 
 (provide 'luis-misc)
