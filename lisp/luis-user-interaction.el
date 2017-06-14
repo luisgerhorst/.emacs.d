c56f38866c40a5f46ba9e5a8ec4ba3418810f673
Luis Gerhorst <privat@luisgerhorst.de>
2017-06-14 15:49:12 +0200

    Disable mouse

--- lisp/luis-user-interaction.el
+++ lisp/luis-user-interaction.el
@@ -2,16 +2,16 @@
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 1)))
 
 ;; Disable Mouse
-;; (dolist (mouse '([mouse-1] [down-mouse-1] [drag-mouse-1]
-;;                  [double-mouse-1] [triple-mouse-1] [mouse-2]
-;;                  [down-mouse-2] [drag-mouse-2] [double-mouse-2]
-;;                  [triple-mouse-2] [mouse-3] [down-mouse-3]
-;;                  [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
-;;                  [mouse-4] [down-mouse-4] [drag-mouse-4]
-;;                  [double-mouse-4] [triple-mouse-4] [mouse-5]
-;;                  [down-mouse-5] [drag-mouse-5] [double-mouse-5]
-;;                  [triple-mouse-5]))
-;;   (global-unset-key mouse))
+(dolist (mouse '([mouse-1] [down-mouse-1] [drag-mouse-1]
+                 [double-mouse-1] [triple-mouse-1] [mouse-2]
+                 [down-mouse-2] [drag-mouse-2] [double-mouse-2]
+                 [triple-mouse-2] [mouse-3] [down-mouse-3]
+                 [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+                 [mouse-4] [down-mouse-4] [drag-mouse-4]
+                 [double-mouse-4] [triple-mouse-4] [mouse-5]
+                 [down-mouse-5] [drag-mouse-5] [double-mouse-5]
+                 [triple-mouse-5]))
+  (global-unset-key mouse))
 
 
 (provide 'luis-user-interaction)
