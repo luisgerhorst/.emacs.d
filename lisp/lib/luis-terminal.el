9c61122e46fd7bfcc8d0d7bc8600dd812033c4df
Luis Gerhorst <privat@luisgerhorst.de>
2017-10-27 00:39:33 +0200

    Paste in term using C-y

--- lisp/lib/luis-terminal.el
+++ lisp/lib/luis-terminal.el
@@ -25,8 +25,25 @@
        (getenv "SHELL")
        "/bin/sh")))
 
+(defun oleh-term-exec-hook ()
+  "Close term buffer after exit.
+
+After you close the terminal, you get a useless buffer with no
+process. It's probably left there for you to have a history of
+what you did. I find it not useful, so here's a way to kill that
+buffer automatically"
+  (let* ((buff (current-buffer))
+         (proc (get-buffer-process buff)))
+    (set-process-sentinel
+     proc
+     `(lambda (process event)
+        (if (string= event "finished\n")
+            (kill-buffer ,buff))))))
+
 (use-package term
-  :bind ("C-c s s" . luis-instant-ansi-term)
+  :bind (("C-c s s" . luis-instant-ansi-term)
+         :map term-raw-map
+         ("C-y" . term-paste))
   :config
   (add-hook 'term-mode-hook #'luis-no-scroll-margin))
 
