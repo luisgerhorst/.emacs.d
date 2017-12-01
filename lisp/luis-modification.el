2f7fd4bbf41f7e08047e5b9b385f58be61f9ce78
Luis Gerhorst <un65esoq@stud.informatik.uni-erlangen.de>
2017-12-01 21:06:46 +0100

    Backport comment-line to pre 25.1

--- lisp/luis-modification.el
+++ lisp/luis-modification.el
@@ -157,6 +157,25 @@
 
 ;;; Commenting
 
+(when (version< emacs-version "25.1")
+  (defun comment-line (n)
+    "Comment or uncomment current line and leave point after it.
+With positive prefix, apply to N lines including current one.
+With negative prefix, apply to -N lines above.
+If region is active, apply to active region instead."
+    (interactive "p")
+    (if (use-region-p)
+        (comment-or-uncomment-region
+         (region-beginning) (region-end))
+      (let ((range
+             (list (line-beginning-position)
+                   (goto-char (line-end-position n)))))
+        (comment-or-uncomment-region
+         (apply #'min range)
+         (apply #'max range)))
+      (forward-line 1)
+      (back-to-indentation))))
+
 (global-set-key (kbd "C-;") #'comment-line)
 (setq comment-style 'extra-line)
 (setq-default comment-column 0)
