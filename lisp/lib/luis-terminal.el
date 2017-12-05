2712542c540488e701109529143931c20972ed86
Luis Gerhorst <luis.gerhorst@fau.de>
2017-12-05 20:12:17 +0100

    Don't explicitly set shell, use $SHELL

--- lisp/lib/luis-terminal.el
+++ lisp/lib/luis-terminal.el
@@ -46,7 +46,6 @@ buffer automatically"
          ;; Otherwise C-y is sent to the shell (terminal yank).
          ("C-y" . term-paste))
   :config
-  (setq explicit-shell-file-name "/usr/local/bin/zsh")
   (add-hook 'term-mode-hook #'luis-term-mode-hook)
   (add-hook 'term-exec-hook #'luis-close-term-buffer-after-exit))
 
