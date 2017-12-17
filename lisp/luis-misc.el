a44195f5b88123479054696ee15f6010bc8f4c2e
Luis Gerhorst <privat@luisgerhorst.de>
2017-12-17 23:52:18 +0100

    Remove broken link

--- lisp/luis-misc.el
+++ lisp/luis-misc.el
@@ -5,9 +5,7 @@
         (get-buffer "*Bookmark List*")))
 
 ;; We use the obsolete frame-restore sice the newer desktop-save does save a
-;; wrong mouse-color on macOS so the mouse looks weird in Emacs. Description
-;; here
-;; https://emacs.stackexchange.com/questions/29124/how-do-i-keep-emacs-from-saving-the-mouse-color-in-the-desktop-file
+;; wrong mouse-color on macOS so the mouse looks weird in Emacs.
 (frame-restore)
 
 (provide 'luis-misc)
