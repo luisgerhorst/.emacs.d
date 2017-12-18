16871337fb58ea9f57a5b5689ada211caf815171
Luis Gerhorst <privat@luisgerhorst.de>
2017-12-18 17:21:48 +0100

    winum instead of ace-window

--- lisp/luis-movement.el
+++ lisp/luis-movement.el
@@ -24,11 +24,20 @@
 
 ;;; Windows
 
-(use-package ace-window
-  :bind ("C-x o" . ace-window)
+(use-package winum
+  :bind (("M-0" . winum-select-window-0-or-10)
+         ("M-1" . winum-select-window-1)
+         ("M-2" . winum-select-window-2)
+         ("M-3" . winum-select-window-3)
+         ("M-4" . winum-select-window-4)
+         ("M-5" . winum-select-window-5)
+         ("M-6" . winum-select-window-6)
+         ("M-7" . winum-select-window-7)
+         ("M-8" . winum-select-window-8)
+         ("M-9" . winum-select-window-9))
+  :demand
   :init
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
-        aw-scope 'frame))
+  (winum-mode))
 
 (use-package winner
   :demand
