cb6c8bc2b3a046a1463034a7b72fb18424719c13
Luis Gerhorst <privat@luisgerhorst.de>
2017-12-19 23:56:57 +0100

    Port changes from subtree

--- lisp/luis-look.el
+++ lisp/luis-look.el
@@ -22,18 +22,27 @@
 
 ;;; Mode Line and Theme
 
-(when (display-graphic-p)
-  (use-package powerline
-    :config
-    (set-face-underline 'mode-line nil)
-    (set-face-underline 'mode-line-inactive nil)
-    ;; Causes incorrect display of active mode line when Emacs is not focused.
-    (remove-hook 'focus-out-hook 'powerline-unset-selected-window)))
+(use-package powerline
+  :config
+  (set-face-underline 'mode-line nil)
+  (set-face-underline 'mode-line-inactive nil)
+  ;; Causes incorrect display of active mode line when Emacs is not focused.
+  (remove-hook 'focus-out-hook 'powerline-unset-selected-window)
 
-(defun luis-theme-set (new background-mode powerline-seperator)
+  (when (not (display-graphic-p))
+    ;; We do this since the patched fonts for powerline don't always work. In
+    ;; iTerm e.g. they may be vertically off as described at
+    ;; https://github.com/powerline/fonts/issues/31
+    (message "powerline terminal")
+    (setq powerline-utf-8-separator-left (string-to-char " ")
+          powerline-utf-8-separator-right (string-to-char " ")
+          ;; The following is default anyway in terminals.
+          powerline-default-separator 'utf-8)))
+
+(defun luis-style-set (new background-mode powerline-seperator)
   (when (display-graphic-p)
-    (setq powerline-default-separator powerline-seperator)
-    (powerline-default-theme))
+    (setq powerline-default-separator powerline-seperator))
+  (powerline-default-theme)
 
   (mapc #'disable-theme custom-enabled-themes)
   (load-theme new t)
@@ -41,23 +50,26 @@
   (setq frame-background-mode background-mode)
   (frame-set-background-mode (selected-frame)))
 
-(defun luis-theme-dark ()
-  (luis-theme-set 'base16-default-dark 'dark nil))
+(defun luis-style-dark ()
+  (require 'solarized)
+  (luis-style-set 'solarized-dark 'dark 'utf-8)
+  (when (not (display-graphic-p))
+    (custom-set-faces
+     '(vertical-border ((t (:background "brightgreen" :foreground "brightgreen")))))))
 
-(defun luis-theme-light ()
-  (luis-theme-set 'adwaita  'light nil))
+(defun luis-style-light ()
+  (luis-style-set 'adwaita 'light nil))
 
-(defvar luis-theme-toggle-state t)
+(defvar luis-style-toggle-state t)
 
 (defun luis-theme-toggle ()
   (interactive)
-  (if luis-theme-toggle-state
-      (luis-theme-light)
-    (luis-theme-dark))
-  (setq luis-theme-toggle-state (not luis-theme-toggle-state)))
+  (if luis-style-toggle-state
+      (luis-style-light)
+    (luis-style-dark))
+  (setq luis-style-toggle-state (not luis-style-toggle-state)))
 
-
-(luis-theme-dark)
+(luis-style-dark)
 
 
 ;;; Whitespaces
