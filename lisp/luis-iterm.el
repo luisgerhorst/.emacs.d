992525bec599e8c9e086786ac757ba87a69bac13
Luis Gerhorst <privat@luisgerhorst.de>
2018-02-13 20:08:05 +0100

    Revert "M-µ redirections for right special chars in International German Dev"
    This reverts commit 867e93510cd5806de662c2dea8f84155d66330a0.

--- lisp/luis-iterm.el
+++ lisp/luis-iterm.el
@@ -11,25 +11,13 @@
     (call-interactively (key-binding key))))
 
 (when (not (display-graphic-p))
-  ;; TODO: Redirections for the all other special chars including those in the
-  ;; number row.
-  (global-set-key (kbd "M-µ c \"") (luis-bound-command (kbd "C-\"")))
-  (global-set-key (kbd "M-µ c '") (luis-bound-command (kbd "C-'")))
-  (global-set-key (kbd "M-µ c ,") (luis-bound-command (kbd "C-,")))
-  (global-set-key (kbd "M-µ c .") (luis-bound-command (kbd "C-.")))
-  (global-set-key (kbd "M-µ c /") (luis-bound-command (kbd "C-/")))
-  (global-set-key (kbd "M-µ c :") (luis-bound-command (kbd "C-:")))
   (global-set-key (kbd "M-µ c ;") (luis-bound-command (kbd "C-;")))
-  (global-set-key (kbd "M-µ c <") (luis-bound-command (kbd "C-<")))
-  (global-set-key (kbd "M-µ c >") (luis-bound-command (kbd "C->")))
-  (global-set-key (kbd "M-µ c ?") (luis-bound-command (kbd "C-?")))
-  (global-set-key (kbd "M-µ c \\") (luis-bound-command (kbd "C-\\")))
-  (global-set-key (kbd "M-µ c {") (luis-bound-command (kbd "C-{")))
-  (global-set-key (kbd "M-µ c |") (luis-bound-command (kbd "C-|")))
-  (global-set-key (kbd "M-µ c }") (luis-bound-command (kbd "C-}")))
+  (global-set-key (kbd "M-µ c .") (luis-bound-command (kbd "C-.")))
+  (global-set-key (kbd "M-µ c :") (luis-bound-command (kbd "C-:")))
   ;; Non-printable bindings, prefixed with an additional µ and using a character
   ;; that fits their function.
-  (global-set-key (kbd "M-µ µ c d") (luis-bound-command (kbd "<C-S-backspace>"))))
+  (global-set-key (kbd "M-µ µ c d") (luis-bound-command (kbd "<C-S-backspace>")))
+  (global-set-key (kbd "M-µ µ c m d") (luis-bound-command (kbd "<C-M-backspace>"))))
 
 
 (provide 'luis-iterm)
