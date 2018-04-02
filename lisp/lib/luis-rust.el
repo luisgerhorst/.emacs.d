59ce18e2f6fa00525fd398d2ceac063e8b7e736b
Luis Gerhorst <privat@luisgerhorst.de>
2018-04-02 13:46:05 +0200

    Rust autocompletion

--- lisp/lib/luis-rust.el
+++ lisp/lib/luis-rust.el
@@ -1,8 +1,19 @@
+(use-package rust-mode
+  :defer t
+  :config
+  (add-hook 'rust-mode-hook #'luis-company-configure-automatic-completion))
+
 (use-package cargo
   :defer t
   :bind (:map
          cargo-minor-mode-map
          ("C-c c" . cargo-process-build)
          ("C-c r" . cargo-process-run)))
+
+(use-package company-race
+  :defer t
+  :init
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends 'company-racer)))
 
 (provide 'luis-rust)
