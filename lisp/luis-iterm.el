5c887317070ed88cd1c02c5fb79c7ee06e6523a7
Luis Gerhorst <luis.gerhorst@fau.de>
2018-04-14 21:56:13 +0200

    iTerm redirection for C-SPC

--- lisp/luis-iterm.el
+++ lisp/luis-iterm.el
@@ -17,7 +17,8 @@
   ;; Non-printable bindings, prefixed with an additional µ and using a character
   ;; that fits their function.
   (global-set-key (kbd "M-µ µ c d") (luis-bound-command (kbd "<C-S-backspace>")))
-  (global-set-key (kbd "M-µ µ c m d") (luis-bound-command (kbd "<C-M-backspace>"))))
+  (global-set-key (kbd "M-µ µ c m d") (luis-bound-command (kbd "<C-M-backspace>")))
+  (global-set-key (kbd "M-µ µ c s") (luis-bound-command (kbd "C-SPC"))))
 
 
 (provide 'luis-iterm)
