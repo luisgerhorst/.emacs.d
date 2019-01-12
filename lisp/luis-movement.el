c223350188f1fa59782fbedba06cb65143eaa31e
Luis Gerhorst <privat@luisgerhorst.de>
2019-01-12 19:41:01 +0100

    remove avy

--- lisp/luis-movement.el
+++ lisp/luis-movement.el
@@ -27,9 +27,6 @@
   :config
   (diminish 'subword-mode))
 
-(use-package avy
-  :bind (("C-:" . avy-goto-char)))
-
 ;; When you visit a file, point goes to the last place where it
 ;; was when you previously visited the same file.
 ;; http://www.emacswiki.org/emacs/SavePlace
