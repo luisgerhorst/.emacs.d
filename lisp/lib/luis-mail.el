9b90361d1edcde1f5826c4ca271b5da1e48b5861
Luis Gerhorst <privat@luisgerhorst.de>
2019-04-21 14:37:48 +0200

    Don't mu4e-view-prefer-html

--- lisp/lib/luis-mail.el
+++ lisp/lib/luis-mail.el
@@ -57,9 +57,6 @@
   ;; (add-hook 'mu4e-compose-mode-hook #'luis-text-wrap-mode)
   ;; (remove-hook 'mu4e-view-mode-hook #'luis-text-wrap-mode)
 
-  ;; Commit 739013d of mu: Setting this to nil makes a lot of mails look bad.
-  (setq mu4e-view-prefer-html t)
-
   (defun luis-mu4e-message-txt-body-will-show (msg)
     (let* ((txt (mu4e-message-field msg :body-txt))
            (html (mu4e-message-field msg :body-html)))
