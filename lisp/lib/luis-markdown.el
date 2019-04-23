2c5fb3dd8c2bed06d4bb6b69cd9be700eb8465a0
Luis Gerhorst <privat@luisgerhorst.de>
2019-04-23 21:21:01 +0200

    add-hook 'markdown-mode-hook #'luis-text-wrap-mode

--- lisp/lib/luis-markdown.el
+++ lisp/lib/luis-markdown.el
@@ -1,9 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
+(require 'luis-text-wrap)
+
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode)))
+         ("\\.markdown\\'" . markdown-mode))
+  :config
+  (add-hook 'markdown-mode-hook #'luis-text-wrap-mode))
 
 (provide 'luis-markdown)
