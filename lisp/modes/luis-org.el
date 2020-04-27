d7d7d0369ce313145984206bb5269d28fd67e7b6
Luis Gerhorst <privat@luisgerhorst.de>
2020-04-27 21:43:06 +0200

    org: C-c a and auto-revert

--- lisp/modes/luis-org.el
+++ lisp/modes/luis-org.el
@@ -2,11 +2,12 @@
 
 (use-package org
   :defer t
-  :bind (("C-c o l" . org-store-link)
-         ("C-c o a" . org-agenda)
+  :bind (("C-c a" . org-agenda)
+         ("C-c o l" . org-store-link)
          ("C-c o c" . org-capture)
          ("C-c o b" . org-switchb))
   :config
+  (add-hook 'org-mode-hook #'auto-revert-mode)
   (add-hook 'org-mode-hook #'auto-fill-mode)
   (add-to-list 'org-latex-packages-alist '("ngerman" "babel" t)))
 
