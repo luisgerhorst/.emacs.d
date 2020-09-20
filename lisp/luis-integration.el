6df33dd10891901d3e60ded3c10c09ee7fb74cf9
Luis Gerhorst <privat@luisgerhorst.de>
2020-09-20 19:15:46 +0200

    xah-open-in-external-app

--- lisp/luis-integration.el
+++ lisp/luis-integration.el
@@ -26,8 +26,6 @@
 ;; server in that instance. This way we always connect to the Emacs deamon when
 ;; using emacsclient and not some random instance started in a terminal.
 
-(global-set-key (kbd "C-c v") #'browse-url-at-point)
-
 ;;; Copy/Paste in Terminal Emacs
 
 (setq select-enable-clipboard t
@@ -74,5 +72,45 @@ evaluated by emacs server."
   :config
   (when openwith-desktop-environment-open
     (openwith-mode t)))
+
+(defun xah-open-in-external-app (&optional fname)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+
+When called in emacs lisp, if fname is given, open that.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-11-04"
+  (interactive)
+  (let* (
+         (file-list
+          (if fname
+              (progn (list fname))
+            (if (string-equal major-mode "dired-mode")
+                (dired-get-marked-files)
+              (list (buffer-file-name)))))
+         (do-it-p (if (<= (length file-list) 5)
+                      t
+                    (y-or-n-p "Open more than 5 files? "))))
+    (when do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda (fpath)
+           (w32-shell-execute "open" fpath))
+         file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda (fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument fpath))))
+         file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda (fpath) (let ((process-connection-type nil))
+                           (start-process "" nil "xdg-open" fpath)))
+         file-list))))))
+
+(global-set-key (kbd "C-c v") #'xah-open-in-external-app)
 
 (provide 'luis-integration)
